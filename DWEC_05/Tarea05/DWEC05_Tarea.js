5540d542befe648c3c9eaa8ec31749643abc9b49
Carlos M. de Lucas <carlanlinux@gmail.com>
2020-02-29 20:26:20 +0100

    Tarea 05 terminada

--- DWEC_05/Tarea05/DWEC05_Tarea.js
+++ DWEC_05/Tarea05/DWEC05_Tarea.js
@@ -9,74 +9,45 @@ window.onload = inicializar;
 //Inicializamos la validación y primero comprobamos si la cookie tiene algún valor en contador.
 function inicializar() {
     // Borramos la cookie si existe anteriormente para empezar a contar desde 0.
-    borrarCookie("contador");
+    crearCookie("contador");
 
     //Hacemos que se cargue la función de cambiar el texto a mayus que se tiene que hacer en todos los casos.
-    aMayus();
+    document.getElementById("nombre").addEventListener("blur", aMayus, false);
+    document.getElementById("apellidos").addEventListener("blur", aMayus, false);
     document.getElementById("enviar").addEventListener('click', validar, false);
-
-
-    //Si existe el valor en la cookie entonces llamamos al evento que se va a encargar de la validación
-    if (document.cookie[contador] != null) {
-
-        //Si no existe el contador en la cookie lo creamos y lo igualamos a 0
-    } else {
-        crearCookie();
-    }
-
+    leerCookie("contador");
 }
 
 
 function validar(evento) {
-    /*
-        En la variable que pongamos aquí gestionaremos el evento por defecto asociado al botón de "enviar"
-        (type=submit) que en este caso lo que hace por defecto es enviar un formulario.
-    */
-    if (validarTexto(this) && validadNIF() && validarProvincia() && validarFecha() && validarHora()
-        && validarTelefono() && validarEmail() && confirm("¿Deseas enviar el formulario?")) {
+    //Sumar 1 intento
+    var intentos = leerCookie("contador") + 1;
+
+    //Creamos la Cookie si no está creada. Si está metemos el nuevo número de intentos.
+    crearCookie("contador", intentos);
+
+    //Mostrarmos el valor de la cookie en el Div
+    document.getElementById("intentos").innerHTML = "Intentos de envío del formulario:" + intentos;
+
+    //Validamos cada uno de los apartados con llamadas a las funciones de validación
+    if (validarTexto(this) && validarEdad() && validadNIF() && validarEmail() && validarProvincia() && validarFechas() && validarTelefonos() && validarHora() && confirm("¿Deseas enviar el formulario?")) {
         return true;
 
     } else {
+        //Cancelamos el envio del formulario
         evento.preventDefault();
-        intentosCookie();
-        return false;
+        return false; // Salimos de la función devolviendo falso
     }
 
 }
 
-
-//Función para crear una coockie con inicializando el contador a 0
-function crearCookie() {
-    document.cookie = "contador=0";
-}
-
-//Cuando se da al botón enviar, si ha habido algún error, ponemos en el div el número de intentos.
-function intentosCookie() {
-    let intentos = parseInt(document.cookie[contador]);
-    document.cookie = "contador=" + intentos++;
-    document.getElementById("intentos").innerHTML = "Intentos de envío del formulario:" + intentos + 1;
-
-}
 
 //Función para poner el texto en mayúsculas cuando se quite el foco
 function aMayus() {
-    //Usamos una función anónima para hacer el cambio de valor
-    document.getElementById("nombre").addEventListener("blur", function () {
         document.getElementById("nombre").value = document.getElementById("nombre").value.toUpperCase();
-    }, false);
-
-    document.getElementById("apellidos").addEventListener("blur", function () {
         document.getElementById("apellidos").value = document.getElementById("apellidos").value.toUpperCase();
-    }, false)
-
 }
 
-function validarNombre() {
-    if (document.getElementById("nombre").value === "" || document.getElementById("apellido").value === "") {
-
-    }
-
-}
 
 function validarTexto(objeto) {
     /* Le pasamos un objeto, el botón de enviar. Con el validarcampostexto(this) hace referencia al objeto dónde se
@@ -89,82 +60,81 @@ function validarTexto(objeto) {
     evitar que nos marquen en rojo cosas que están correctas y que pasan la validación. */
     for (let i = 0; i < formulario.elements.length; i++) {
         formulario.elements[i].className = "";
-
-    }
-
-    // Recorrer todos los elementos del formulario, buscando los que son de tipo texto y validar que contengan valor.
-    for (let i = 0; i < formulario.elements.length; i++) {
-
+        //buscando los que son de tipo texto y validar que contengan valor.
         if (formulario.elements[i].type === "text" && formulario.elements[i].value === "") {
             //Pintamos mensaje en el contenedor
-            document.getElementById("errores").innerHTML = "El campo" + formulario.elements[i].name +
-                "no puede estar en blanco.";
+            document.getElementById("errores").innerHTML = "El campo " + formulario.elements[i].name + " no puede estar en blanco.";
             //Asignamos clase error para que se active el CSS en rojo
             formulario.elements[i].className = "error";
             //Ponemos el foco en el elemento en cuestión que no pasa la validación.
             formulario.elements[i].focus();
             return false;
         }
-
-        // Aprovechamos y dentro de la función validamos también la edad buscando el elemento
-        else if (formulario.elements[i].id === "edad") {
-            //Si vemos que no contiene un número o que sea negativo o mayor de 120 no lo damos por válido.
-
-            if (isNaN(formulario.elements[i].value) || formulario.elements[i].value < 0 || formulario.elements[i].value > 120) {
-                //Pintamos mensaje en el contenedor
-                document.getElementById("errores").innerHTML = "El campo" + formulario.elements[i].name + " tiene" +
-                    "valores incorrectos. Edad debe estar comprendida entre 0 y 120";
-                //Asignamos clase error para que se active el CSS en rojo
-                formulario.elements[i].className = "error";
-                //Ponemos el foco en el elemento en cuestión que no pasa la validación.
-                formulario.elements[i].focus();
-                return false;
-            }
-        } else {
-            document.getElementById("email").className = "";
-            return true;
-        }
     }
+    //Si ha llegado aquí quiere decir que todos los campos se han validado correctamente
+    return true;
+}
+
+
+function validarEdad() {
+    //Si no es un número y la edad es menor de 0 y mayor de 120 años da error
+    if (isNaN(document.getElementById("edad").value) || document.getElementById("edad").value < 0 ||
+        document.getElementById("edad").value > 120) {
+        //Pintamos mensaje en el contenedor
+        document.getElementById("errores").innerHTML = "Error: Edad debe estar comprendida entre 0 y 120";
+        //Asignamos clase error para que se active el CSS en rojo
+        formulario.elements[i].className = "error";
+        //Ponemos el foco en el elemento en cuestión que no pasa la validación.
+        formulario.elements[i].focus();
+        return false;
+    }
+    // Si llega aquí la edad está dentro de los límites y se le deja la clase en blanco
+    document.getElementById("edad").className = "";
+    return true;
 
 }
 
 
+
 function validadNIF() {
     //Metemos el valor del campo dni en una variable para luego llevar a cabo el test.
-    let dni = document.getElementById("dni").value;
-    //Creamos el patrón a seguir con: 8 caracteres decimales, seguidos de un guión y de un caracter no dígito
-    let patron = /^\d{8}\s-\[A-Z]$/;
+    let dni = document.getElementById("nif").value;
+    //Creamos el patrón a seguir con: 8 caracteres decimales, seguidos de un guión y de un caracter de A a Z
+    let patron = /^\d{8}-[A-Z]$/;
     //Compronamos que cuando no coincida el valor con el patrón salte el error y ponga el foco.
     if (!patron.test(dni)) {
-        document.getElementById("errores").innerHTML = "El número de DNI introducido no es correcto";
-        document.getElementById("dni").className = "error";
-        document.getElementById("dni").focus();
+        document.getElementById("errores").innerHTML = "Error: El número de DNI introducido no es correcto";
+        document.getElementById("nif").className = "error";
+        document.getElementById("nif").focus();
         return false
-    } else {
-        //Quitamos el elemento de la clase error (Por si previamente había dado error)
-        document.getElementById("email").className = "";
-        return true;
     }
+    //si llega aquí es que pasa la validación
+    //Quitamos el elemento de la clase error (Por si previamente había dado error)
+    document.getElementById("nif").className = "";
+    return true;
 }
 
 function validarEmail() {
     //Metemos el valor del mail en una variable
     let email = document.getElementById("email").value;
     /*    Creamos el patrón con la expresión regular de forma que:
-        /\S+@ --> Metemos más de una cadena de texto hasta llegar a una arroba
-        \S+ -->.Se mete tra cadena de texto hasta llegar a un punto
-        \S+/ --> Se sigue con otra cadena de texto después del punto */
-    let patron = /\S+@\S+\.\S+/;
+    [\w-.]{2,} --> 2 ó más caracteres alfanuméricos, incluyendo el guión y el punto, seguido de un símbolo @
+    ([\w-]{2,}\.) -->  2 ó más caracteres alfanuméricos incluido el guión y finalizando en un punto.
+    +([\w-]{2,4})$ -->  Terminará con 2 a 4 caracteres alfanuméricos incluidos el guión
+     */
 
+    let patron = /^[\w-.]{2,}@([\w-]{2,}\.)+([\w-]{2,4})$/;
+    //Si no se cumple el patrón
     if (!patron.test(email)) {
-        document.getElementById("errores").innerHTML = "El emaail introducido no es correcto";
+        document.getElementById("errores").innerHTML = "Error: El email introducido no es correcto";
         document.getElementById("email").className = "error";
         document.getElementById("email").focus();
         return false;
-    } else {
-        document.getElementById("email").className = "";
-        return true;
     }
+    //si llega aquí es que pasa la validación
+    //Quitamos el elemento de la clase error (Por si previamente había dado
+    document.getElementById("email").className = "";
+    return true;
 }
 
 function validarProvincia() {
@@ -175,67 +145,99 @@ function validarProvincia() {
         if (document.getElementById("provincia").options[i].selected) {
             document.getElementById("provincia").className = "";
             return true;
-        } else {
-            document.getElementById("errores").innerHTML = "Debe seleccionar una provincia";
-            document.getElementById("provincia").className = "error";
-            document.getElementById("provincia").focus();
-
         }
     }
+    //si llega aquí es que no hay seleccionada ninguna provincia
+    document.getElementById("errores").innerHTML = "Error: Debe seleccionar una provincia";
+    document.getElementById("provincia").className = "error";
+    document.getElementById("provincia").focus();
     return false;
 }
 
-function validarFecha() {
-    /*    No se me ocurre cómo validarlo con regExp, además, lo que he encontrado por internet eran unos tochos bastante
-        ininteligibles. Si se pudiera tocar el HMTL pondría type fecha al campo y evitaba esta validación. */
-    let patron = /\d{2}\s-\d{2}\s-\d{4}/;
+function validarFechas() {
+    /*
+       /^((0?[1-9])|(1\d)|(2\d)|(3[0-1])) --> Debe empezar por 0 (no obligatorio) y un número del 1 al 9, ó por 1 y un
+       dígito decimal, ó por 2 y un dígito decimal, ó por 3 y un dígito entre el 0 y el 1.
+       [-|\/] --> Acepta un guión o una barra inclinada.
+       ((0?[1-9])|(1[0-2])) --> 0 (no obligatorio) y un dígito entre el 1 y el 9, ó un 1 seguido de dígitos entre el 0 y el 2
+       [-|\/] --> Acepta un guión o una barra inclinada.
+       ([1-2]\d{3})$/ --> Un dígito entre el 1 y el 2 (por 1000 o 2000) seguido de 3 dígitos numéricos
+     */
+    patron = /^((0?[1-9])|(1\d)|(2\d)|(3[0-1]))[-|\/]((0?[1-9])|(1[0-2]))[-|\/]([1-2]\d{3})$/;
 
+    //si no cumple el patrón:
     if (!patron.test(document.getElementById("fecha").value)) {
         document.getElementById("errores").innerHTML = "La fecha introducida no es correcta";
         document.getElementById("fecha").className = "error";
         document.getElementById("fecha").focus();
         return false;
 
-    } else {
-        document.getElementById("email").className = "";
-        return true;
     }
+    //si llega aquí es que pasa la validación
+    //Quitamos el elemento de la clase error (Por si previamente había dado
+    document.getElementById("email").className = "";
+    return true;
 }
 
-function validarTelefono() {
-    let patron = /\d{9}/;
+function validarTelefonos() {
+    /* Los teléfonos empiezan en ES por 9 o 6 y está formado por 9 numeros
+    /^[69]\ --> Empieza por 6 o 9
+    \d{8}$/ --> 8 números.
+    */
+    let patron = /^[69]\d{8}$/;
+
     if (!patron.test(document.getElementById("telefono").value)) {
-        document.getElementById("errores").innerHTML = "El teléfono introducido es incorrecta";
+        document.getElementById("errores").innerHTML = "Error: El teléfono introducido es incorrecta";
         document.getElementById("telefono").className = "error";
         document.getElementById("telefono").focus();
         return false;
-    } else {
-        document.getElementById("telefono").className = "";
-        return true;
     }
+    //si llega aquí es que pasa la validación
+    //Quitamos el elemento de la clase error (Por si previamente había dado
+    document.getElementById("telefono").className = "";
+    return true;
 }
 
 function validarHora() {
-    let patron = /\d{2}\s:\d{2}/;
+    /*
+    /^(0?[1-9]|1\d|2[0-3]) --> Comenzamos con un 0 (no obligatorio) y un dígito del 1 al 9, ó un 1 y un dígito numérico,
+    ó un 2 y un dígito de 0 a 3,
+    : --> dos puntos
+    ([0-5]?\d)$/ --> un dígito entre el 0 y el 5 (no obligatorio)
+    */
+
+    let patron = /^(0?[1-9]|1\d|2[0-3]):([0-5]?\d)$/;
     if (!patron.test(document.getElementById("hora").value)) {
-        document.getElementById("error").innerHTML = "La hora introducida no es correcta";
+        document.getElementById("error").innerHTML = "Error: La hora introducida no es correcta";
         document.getElementById("hora").className = "error";
         document.getElementById("hora").focus();
         return false;
     }
-    document.getElementById("telefono").className = "";
+    //si llega aquí es que pasa la validación
+    //Quitamos el elemento de la clase error (Por si previamente había dado
+    document.getElementById("hora").className = "";
     return true;
 
 }
 
 //función para crear la cookie contador
-crearCookie();
-{
-    document.cookie = "contador=0";
+function crearCookie(nombre, intentos) {
+    if (intentos) {
+        document.cookie = nombre + "=" + intentos;
+    } else {
+        document.cookie = nombre + "=0";
+    }
+
 }
 
-//función para leer la cookkie contador:
-leerCookie(nombre);
-{
+//Leemos el nombre de la cookie
+function leerCookie(nombre) {
+    //Guardamos el nombre de la cookie seguido del igual
+    let cookie = document.cookie;
+
+    //Devolvemos el número de intentos haciendo split en la cookie
+    //Se devuelve el array 1 porque al separar,  la primera posición del array (0) está vacía, no hay nada y por eso
+    //Tenemos que devolver la posición 1.
+    return parseInt(cookie.split(nombre + "=")[1]);
 
 }
